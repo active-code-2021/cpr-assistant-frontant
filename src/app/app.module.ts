64e6642881b15b9b471420f4822b444d8c5103a2
MalkaLando <83667698+MalkaLando@users.noreply.github.com>
2021-08-30 12:24:11 +0300

    initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { SimpleComponent } from './modules/simple/simple.component';
+import { SimpleService } from './modules/simple.service';
+@NgModule({
+  declarations: [
+    AppComponent,
+    SimpleComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    BrowserAnimationsModule
+  ],
+  providers: [SimpleService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
